bec6ad6f888a5de04075d7847422ff5f8839676a
Suraj-Expand-QA <suraj@expand.network>
2024-09-02 23:17:33 +0530

    Minor changes in Homepage and loginpage.ts

--- pageObject/UI/adminDashboard/homePage.ts
+++ pageObject/UI/adminDashboard/homePage.ts
@@ -3,14 +3,13 @@ import { Page, expect } from "@playwright/test";
 export class homePage {
 
     protected readonly page: Page
+    protected readonly Nodes_Loc = '//h1[@data-testid="test-typography" and contains(text(),"Nodes")]'
     constructor(page: Page) {
         this.page = page
     }
 
     async isUserOnNodePage() {
-
-        expect(await this.page.locator('//h1[@data-testid="test-typography" and contains(text(),"Nodes")]').isVisible()).toBeTruthy()
-
+        expect(await this.page.locator(this.Nodes_Loc).isVisible()).toBeTruthy()
     }
 
 }    
--- pageObject/UI/adminDashboard/loginPage.ts
+++ pageObject/UI/adminDashboard/loginPage.ts
@@ -5,6 +5,10 @@ import { takeScreenshot } from "../../../libs/screenShot"
 export class loginPage {
     protected readonly page: Page
     protected readonly dashPage_loc = '//span[@data-testid="test-typography" and contains(text(),"Welcome")]'
+    protected readonly email_Loc = '//span[@title="Email"]'
+    protected readonly password_loc = '//span[@title="Password"]'
+    protected readonly rememberMe_Loc = 'test-checkbox-label'
+    protected readonly submit_loc = 'test-button'
 
     constructor(page: Page) {
         this.page = page
@@ -15,10 +19,12 @@ export class loginPage {
 
     async login(username: string, password: string) {
         const library = new decodeEncode();
-        await this.page.locator('//span[@title="Email"]').fill(await library.encode(username))
+        await this.page.locator(this.email_Loc).fill(await library.decode(username))
         await takeScreenshot(this.page, "fill-email");
-        await this.page.locator('//span[@title="Password"]').fill(await library.encode(password))
+        await this.page.locator(this.password_loc).fill(await library.decode(password))
         await takeScreenshot(this.page, "fill-password");
+        await this.page.getByTestId(this.rememberMe_Loc).click()
+        await this.page.getByTestId(this.submit_loc).click()
         // await this.page.waitForTimeout(20000)
         // await this.page.context().storageState({ path: 'libs/auth.json' })
 
